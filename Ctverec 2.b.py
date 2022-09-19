861b270c6673e57bacc8a3a6ce4b6329eb8d6f3e
PetrZukal <113590928+PetrZukal@users.noreply.github.com>
2022-09-19 09:31:40 +0200

    Create Ctverec 2.b.py

---
+++ Ctverec 2.b.py
@@ -0,0 +1,15 @@
+from turtle import forward, left, right
+
+for i in range(4):
+    forward(100)
+    left(90)
+
+for i in range(1):
+    forward(200)
+
+for i in range(4):
+    forward(100)
+    left(90)
+
+
+
