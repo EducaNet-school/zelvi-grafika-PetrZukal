a6657e5fa96e504aa961c93cbe2587100867e759
PetrZukal <113590928+PetrZukal@users.noreply.github.com>
2022-10-16 17:52:37 +0200

    Seznamy
    Dobrý den, odesílám 3 příklady. Bohužel 3 příklad mi nevyšel a nedokázal jsem na něj přijít.

---
+++ seznamy zacatek 1.py
@@ -0,0 +1,13 @@
+prvnicislo = int(input("Zadejte vase prvni cislo"))
+poslednicislo = int(input("Zadejte vase posledni cislo"))
+
+if prvnicislo<poslednicislo:
+    for cislo in range(prvnicislo, poslednicislo +1):
+        if cislo % 2 != 0:
+            print(cislo, end = "")
+else:
+    print("Zkus to znovu")
+
+
+
+
---
+++ seznamy zacatek 2.py
@@ -0,0 +1,11 @@
+
+print ("Malá násobilka")
+
+cislo = int(input("Zadejte číslo které bude od 2 do 10:"))
+
+if cislo <11 and cislo >=2:
+    for j in range(2,11):
+        print(cislo, 'x', j, '=', cislo*j)
+
+else:
+    print("Zkus to znovu")
